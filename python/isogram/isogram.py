fcec9e2a8b02b0d922ab2c9530e03a8c37d588e4
cloggy45 <cloggy45@googlemail.com>
2018-02-04 07:09:52 +0000

    init commit

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,3 +1,10 @@
 def is_isogram(string):
+    newString  = ''.join(string)
+    print(newString)
     if string == "":
-	return True	
+		return True
+    for letter in newString:
+		if newString.find(letter) == -1:
+			return True
+		else:
+			return False	
