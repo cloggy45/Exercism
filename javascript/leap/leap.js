a49bc03818baf90d5a011d81a601869e034a9609
cloggy45 <cloggy45@googlemail.com>
2018-04-28 04:39:48 +0000

    Finished leap year exercise

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -3,18 +3,20 @@
 // convenience to get you started writing code faster.
 //
 
-var Year = function (year = "") {
+var Year = function (year) {
     this._year = year;
 	
 };
 
 Year.prototype.isLeap = function () {
-    const isLeapYear = [
-	this._year !== 2015,	    
-        this._year % 4 === 0,
-	this._year % 100 === 0
-    ].reduce((acc, cur) => acc && cur, true);
-    return isLeapYear;
+    const year = this._year;
+    
+    const conditions = [
+        year % 4 === 0,
+        year % 100 !== 0
+        ]
+    
+    return conditions.reduce((a,b) => a && b, true) || (year % 400 == 0);
 };
 
 module.exports = Year;
